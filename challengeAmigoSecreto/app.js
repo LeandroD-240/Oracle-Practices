56d3f6c8200c453bb27aa75f5b74ce79458f0b02
Leandro Díaz Calderón <leandroernestodiazcalderon@gmail.com>
2025-02-04 20:06:06 +0000

    Development the game

--- challengeAmigoSecreto/app.js
+++ challengeAmigoSecreto/app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friends = [];
+
+function agregarAmigo() {
+    let addfriend = document.getElementById("amigo").value;
+    //addition = document.getElementById("listaAmigos").innerText = addfriend;
+    if (addfriend == "") {
+        alert("Please, put a name");
+    } else {
+        friends.push(addfriend);
+        document.getElementById("amigo").value = "";
+        //document.getElementById("listaAmigos").innerHTML = friends;
+    }
+}
